768d700ede9e6e0b49b2461b99e6d3b2df584417
saccho <strk956@gmail.com>
2018-05-21 04:19:43 +0900

    Updated app.py

---
+++ app_test.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from cafebot.bot import Bot
+
+def main():
+    port = int(os.getenv("PORT", 5000))
+    # # create tmp dir for download content
+    # make_static_tmp_dir()
+    app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
