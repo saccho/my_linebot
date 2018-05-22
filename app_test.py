cc3e3408088a973bbe0b030fe882842d0d0c2368
saccho <strk956@gmail.com>
2018-05-22 23:59:31 +0900

    test

--- app_test.py
+++ app_test.py
@@ -3,10 +3,8 @@
 from cafebot.bot import Bot
 
 def main():
-    port = int(os.getenv("PORT", 5000))
-    # # create tmp dir for download content
-    # make_static_tmp_dir()
-    app.run(host="0.0.0.0", port=port)
+    bot = Bot()
+    bot.run()
 
 if __name__ == "__main__":
     main()
